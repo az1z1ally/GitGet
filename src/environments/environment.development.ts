51c44f0ac28596167ec72ab3bacb1548e0f04e16
az1z1ally <albyally5@gmail.com>
2024-05-06 22:00:23 +0300

    Fixed typo in dev environment varibales

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   config:{
     apiUrl:'https://api.github.com/repos',
-    frontEndURL: 'http://localhost/4200'
+    frontEndURL: 'http://localhost:4200'
   }
 };
